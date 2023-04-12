398d5014c073a7b6704fbd3830630a0c4c8103ef
Matayay Karuna <matayay2@gmail.com>
2023-04-12 02:47:49 -0600

    first commit

---
+++ src/firebase-config.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const apiKey = import.meta.env.VITE_apiKey;
+const authDomain = import.meta.env.VITE_authDomain;
+const projectId = import.meta.env.VITE_projectId;
+const storageBucket = import.meta.env.VITE_storageBucket;
+const messagingSenderId = import.meta.env.VITE_messagingSenderId;
+const appId = import.meta.env.VITE_appId;
+
+const firebaseConfig = {
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
